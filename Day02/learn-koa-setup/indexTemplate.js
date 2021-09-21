5175ae24056159ba100bf5b45a7542d1890e4a04
hemengting <luna630h@163.com>
2021-09-21 22:22:41 +0800

    feat koa-setup 使用ejs模板生成index.js

--- Day02/learn-koa-setup/indexTemplate.js
+++ Day02/learn-koa-setup/indexTemplate.js
@@ -1,5 +1,11 @@
+import ejs from "ejs";
+import fs from "fs"
+
 export function createIndexTemplate() {
-    return `
-        const Koa = require("koa")
-    `
+    // const template = fs.readFileSync("./template/index.ejs").toString();
+    const template = fs.readFileSync("./template/index.ejs", "utf-8");
+    const code = ejs.render(template);
+    console.log(code);
+
+    return code;
 }
--- Day02/learn-koa-setup/note.md
+++ Day02/learn-koa-setup/note.md
@@ -1,8 +1,14 @@
-package.json中配置
-"type": "module"可以使用esm模块规范
+-------------koa setup----------------
+package.json中配置`"type": "module"`可以使用esm模块规范
 
 rd /S <文件夹名>删除非空文件夹
 
 优化：getRootPath
 
 开发思想 - 小步骤
+
+ejs Embedded JavaScript templates
+
+问题驱动
+1.手动创建的步骤，主流程happy path -> 翻译成代码
+2.细化分支，扩展每个步骤 -> 引入模板
