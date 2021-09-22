9a39840d997bdc595c26910cec256bc07b97383f
hemengting <luna630h@163.com>
2021-09-22 11:01:06 +0800

    feat 添加logo

--- Day03/react-app/src/App.js
+++ Day03/react-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import logo from "./assets/logo.svg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>
+        <a href="https://ant.design/index-cn" id="logo">
+          <img alt="logo" src={logo}/>
+          Ant Design
         </a>
-      </header>
+      </h1>
     </div>
   );
 }
